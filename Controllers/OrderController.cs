a8f62575fad3c2b45a39de49d8ec8a4b66d78c34
datnt19213 <91647943+datnt19213@users.noreply.github.com>
2022-03-12 16:00:11 +0700

    Upload order view controller and model
    Upload order code includes view controller and model

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using FPTBookProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FPTBookProject.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: ManageCategory
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == "Admin" && Session["UserName"] != null)
+            {
+                var orders = db.Orders.ToList();
+                return View(orders);
+            }
+            else
+            {
+                return RedirectToAction("Log_in", "Account");
+            }
+        }
+
+        public ActionResult DeleteOrder(int? id)
+        {
+            Order tmp = db.Orders.ToList().Find(x => x.OrderId == id);
+            if (tmp != null)
+            {
+                db.Orders.Remove(tmp);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
